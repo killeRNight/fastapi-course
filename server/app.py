965b4b58c4970e81148e64f49804fcbe092c7472
Ainur Kazykhanov <ainur.kazykhanov@gmail.com>
2022-11-15 13:49:49 +0300

    updated repo

--- server/app.py
+++ server/app.py
@@ -35,3 +35,8 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+def index():
+    return {"message": "Welcome friend!"}
